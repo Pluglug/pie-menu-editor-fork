4fbaf80cbadae39ca6b05630ac7488eea0a839b9
Pluglug <upward571103@gmail.com>
2025-07-21 01:00:00 +0900

    chore: Update version to 1.18.9 and rename to "Pie Menu Editor Fork"
    - Version bumped from 1.18.8 to 1.18.9
    - Display name changed from "Pie Menu Editor" to "Pie Menu Editor Fork"
    - Helps users distinguish between official PME (roaoao) and fork maintenance
    - No functional changes to addon behavior

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 bl_info = {
-    "name": "Pie Menu Editor",
+    "name": "Pie Menu Editor Fork",
     "author": "roaoao, pluglug",
-    "version": (1, 18, 8),
+    "version": (1, 18, 9),
     "blender": (3, 2, 0),
     "warning": "",
     "tracker_url": "http://blenderartists.org/forum/showthread.php?392910",
